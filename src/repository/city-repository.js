87f6ce543e87a47924b01409c1608d36a22865b5
supersharmapunit <punit2000sharma@gmail.com>
2022-12-17 13:44:50 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,7 +4,9 @@ class CityRepository {
     async  createCity({ name }){
         try {
             const city = await City.create({ name });
+            return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -15,9 +17,35 @@ class CityRepository {
                 where : {
                     id: cityId
                 }
-            }) 
+            }) ;
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where : {
+                    id : cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 }
