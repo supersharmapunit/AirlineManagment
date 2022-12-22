3ecdcc4f1a3ccd576417add526e4a3551c787d23
supersharmapunit <punit2000sharma@gmail.com>
2022-12-23 00:22:20 +0530

    Added flight curd with few filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,7 @@ const flightService = new FlightService();
 
 const create = async(req, res) => {
     try {
-        const flight = await flightService.createFlight(req.query);
+        const flight = await flightService.createFlight(req.body);
         return res.status(201).json({
             data: flight,
             success: true,
@@ -22,6 +22,27 @@ const create = async(req, res) => {
     }
 }
 
+const getAll = async (req, res) => {
+    try {
+        const response = await flightService.getAllFlightData(req.query);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: 'Successfully fetched the flights',
+            err: {}
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the flights',
+            err: error
+        });
+    }
+}
+
 module.exports = {
     create,
+    getAll,
 }
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -24,8 +24,14 @@ class FlightService {
         }
     }
 
-    async getFlightData() {
-        // todo
+    async getAllFlightData(data) {
+        try {
+            const flights = await this.flightRepository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Something went wrong at the service layer");
+            throw { error };
+        }
     }
 }
 
